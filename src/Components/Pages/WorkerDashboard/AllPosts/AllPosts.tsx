8aad98b17f493cc670a93ee2407f265c8b062cb7
ayoubbenjaa <ayoubbenjaa642@gmail.com>
2024-01-26 15:45:44 +0100

    Mr Benjaa

--- src/Components/Pages/WorkerDashboard/AllPosts/AllPosts.tsx
+++ src/Components/Pages/WorkerDashboard/AllPosts/AllPosts.tsx
@@ -1,8 +1,8 @@
+import ToolType from "../ToolsTypes/ToolType";
 import { HiDotsHorizontal } from "react-icons/hi";
 import { useRef, RefObject , useState } from "react";
 import { MdNavigateNext } from "react-icons/md";
 import { GrFormPrevious } from "react-icons/gr";
-import ToolType from "../ToolsTypes/ToolType";
  const data = [
   {img:"imgUsed/wall.jpg", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus molestias beatae unde amet molestiae Dicta.",date:"12/02/2024"},    {img:"imgUsed/wall.jpg", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus molestias beatae unde amet molestiae Dicta."},
   {img:"imgUsed/wall.jpg", text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus molestias beatae unde amet molestiae Dicta.",date:"12/02/2024"},
@@ -18,10 +18,10 @@ import ToolType from "../ToolsTypes/ToolType";
 
 const card_width = 300;
 const card_height = 300;
+ 
 
 
-
-const AllPosts = () => {
+const AllPosts  = () => {
   const containerRef: RefObject<HTMLDivElement> = useRef(null);
   const [scrollEnabled, setScrollEnabled] = useState(false);
   const handleScroll = (amount: number) => {
@@ -33,10 +33,8 @@ const AllPosts = () => {
     }
   };
 
-  
   return (
-    <div className="container m-auto" 
-    >
+    <div className="container m-auto" >
       <div ref={containerRef} className=" flex justify-center items-center"
         style={{
           scrollBehavior: "smooth",
@@ -46,18 +44,17 @@ const AllPosts = () => {
         
         {data.map((item, index) => (
           <div 
-            
             key={index} style={{minWidth: card_width, minHeight: card_height}} className=" m-10 relative shadow-xl rounded-xl    ">
             <img className="rounded-t-xl" src={item.img} alt="img" />
             <div className="border-x-2 border-gray-400 px-2 pb-2 border-b-2 rounded-b-xl">
               <p className="p-2">{item.text}</p>
               <div className="flex  items-center justify-between">
                 {item.date}
-                <HiDotsHorizontal />
+                <HiDotsHorizontal className="text-4xl mr-5 text-teal500" />
               </div>
 
             </div>
-                <ToolType/>
+                <ToolType isEnable={true}/>
           </div>
         ))}
       </div>
--- src/Components/Pages/WorkerDashboard/ToolsTypes/ToolType.tsx
+++ src/Components/Pages/WorkerDashboard/ToolsTypes/ToolType.tsx
@@ -5,29 +5,34 @@ import { MdOutlineFeaturedPlayList } from "react-icons/md";
 import { IoCloseSharp } from "react-icons/io5";
 import { useState } from "react";
 
+ interface IPROPS  {
+     isEnable: Boolean,
+ }
 
-
-
-const ToolType = () => {
+const ToolType: React.FC<IPROPS> = () => {
    
     const [showToolType , setShowToolType] = useState("block");
-
+    //   if(isEnable){
+    //       setShowToolType("block");
+    //   }else{
+    //      setShowToolType("none")
+    //   }
   return (
    
-            <div 
-                style={{
-                    display: showToolType,
-                }}
-            className="absolute flex-col w-52 ml-14 -mt-[326px]  bg-teal500 bg-opacity-90  border-gray-300 p-4 rounded-lg ">
-                <button className="flex justify-end  text-white">
-                    <IoCloseSharp className=" text-xl -mt-3 mb-4" />
-                </button >
-                <div className="flex flex-col ">
-                    <button className=" flex items-center ml-14  pt-2 text-lg  text-white rounded-md"><MdOutlineFeaturedPlayList  className=" " />Display</button>
-                    <button className="flex items-center ml-14  pt-2 text-lg  text-white rounded-md"><BiEdit className="mr-2" /> Edit</button>
-                    <button className=" flex items-center ml-14 pt-2 text-lg   text-white rounded-md "><RiDeleteBin5Line className="mr-2" />Delete</button>
-                </div>
-            </div>
+    <div   
+        style={{display:showToolType}}
+        className="absolute flex-col w-52 ml-14 -mt-[326px]  bg-teal500 bg-opacity-90  border-gray-300 p-4 rounded-lg "
+        >
+       
+        <button className="flex justify-end  text-white">
+            <IoCloseSharp className=" text-xl -mt-3 mb-4" />
+        </button >
+        <div className="flex flex-col ">
+            <button className=" flex items-center ml-14  pt-2 text-lg  text-white rounded-md"><MdOutlineFeaturedPlayList  className=" " />Display</button>
+            <button className="flex items-center ml-14  pt-2 text-lg  text-white rounded-md"><BiEdit className="mr-2" /> Edit</button>
+            <button className=" flex items-center ml-14 pt-2 text-lg   text-white rounded-md "><RiDeleteBin5Line className="mr-2" />Delete</button>
+        </div>
+    </div>
   )
 }
 
